12e982254edac5b0da103c40d1d903215310196c
henrik <henrik@unist.ac.kr>
2022-05-11 12:25:11 +0900

    Fix collection type of Articles and Patents from list to ChainMap

--- src/Papers/merge.py
+++ src/Papers/merge.py
@@ -1,11 +1,13 @@
 import gzip
 import pickle
+from collections import ChainMap
 from more_itertools import pairwise
+from multiprocessing import Pool
 from mypathlib import PathTemplate
 from .main import W_FILE as R_FILE
 
 
-W_FILE = PathTemplate('$rsrc/pdata/paper/article_$index.pkl')
+W_FILE = PathTemplate('$rsrc/pdata/paper/paper_$index.pkl.gz')
 
 
 def read_paper(number):
@@ -14,15 +16,17 @@ def read_paper(number):
 
 
 def merge_paper(index, start, stop):
-    res = [read_paper(number) for number in range(start, stop)]
+    papers = iter(read_paper(number) for number in reversed(range(start, stop)))
+    res = ChainMap(*papers)
     with gzip.open(W_FILE.substitute(index=index), 'wb') as file:
         pickle.dump(res, file)
 
 
 def main_paper():
-    splits = [i for i in range(1, 1115, 40)] + [1115]
-    for index, (start, stop) in enumerate(pairwise(splits)):
-        merge_paper(index, start, stop)
+    splits = [i for i in range(1, 1115, 10)] + [1115]
+    args = [(index, start, stop) for index, (start, stop) in enumerate(pairwise(splits))]
+    with Pool(5) as p:
+        p.starmap(merge_paper, args)
 
 
 if __name__ == '__main__':
--- src/Patents/merge.py
+++ src/Patents/merge.py
@@ -1,11 +1,13 @@
 import gzip
 import pickle
+from collections import ChainMap
 from more_itertools import pairwise
+from multiprocessing import Pool
 from mypathlib import PathTemplate
 from .main import W_FILE as R_FILE
 
 
-W_FILE = PathTemplate('$rsrc/pdata/patent/patent_$index.pkl')
+W_FILE = PathTemplate('$rsrc/pdata/patent/patent_$index.pkl.gz')
 
 
 def read_patent(number):
@@ -14,15 +16,17 @@ def read_patent(number):
 
 
 def merge_patent(index, start, stop):
-    res = [read_patent(number) for number in range(start, stop)]
+    patents = iter(read_patent(number) for number in reversed(range(start, stop)))
+    res = ChainMap(*patents)
     with gzip.open(W_FILE.substitute(index=index), 'wb') as file:
         pickle.dump(res, file)
 
 
 def main_patent():
     splits = [i for i in range(0, 10033, 90)] + [10033]
-    for index, (start, stop) in enumerate(pairwise(splits)):
-        merge_patent(index, start, stop)
+    args = [(index, start, stop) for index, (start, stop) in enumerate(pairwise(splits))]
+    with Pool(5) as p:
+        p.starmap(merge_patent, args)
 
 
 if __name__ == '__main__':
