9d041a05288d9694e4e0d88f76e0df9a80409ed9
henrik <henrik@unist.ac.kr>
2022-05-13 14:45:59 +0900

    Modify Papers.merge.py according to type change of Articles container
    Previously, .merge.py simply bound multiple (10 for paper, 90 for patent) containers into one ChainMap.
    From now on, .mergy.py merges multiple containers into one ChainMap while leaving articles that have any replica.
    Those replicas are merged into the last file.

--- src/Papers/merge.py
+++ src/Papers/merge.py
@@ -5,29 +5,45 @@ from more_itertools import pairwise
 from multiprocessing import Pool
 from mypathlib import PathTemplate
 from .main import W_FILE as R_FILE
+from .replicas import Replica
 
 
 W_FILE = PathTemplate('$rsrc/pdata/paper/paper_$index.pkl.gz')
+replicas = Replica(load=True)
 
 
-def read_paper(number):
+def read_articles(number):
     with gzip.open(R_FILE.substitute(number=number), 'rb') as file:
         return pickle.load(file)
 
 
-def merge_paper(index, start, stop):
-    papers = iter(read_paper(number) for number in reversed(range(start, stop)))
+def read_articles_not_repeated(number):
+    return {x.pmid: x for x in read_articles(number) if x not in replicas}
+
+
+def merge_and_write(index, start, stop):
+    papers = iter(read_articles_not_repeated(number) for number in reversed(range(start, stop)))
     res = ChainMap(*papers)
+    assert len(res) == sum(len(x) for x in res.maps)
     with gzip.open(W_FILE.substitute(index=index), 'wb') as file:
         pickle.dump(res, file)
 
 
-def main_paper():
+def append_repeated(index):
+    with open(W_FILE.substitute(index=index)) as file:
+        chain = pickle.load(file)
+    chain.maps.append(dict(replicas))
+    with open(W_FILE.substitute(index=index), 'wb') as file:
+        pickle.dump(chain, file)
+
+
+def main():
     splits = [i for i in range(1, 1115, 10)] + [1115]
     args = [(index, start, stop) for index, (start, stop) in enumerate(pairwise(splits))]
     with Pool(5) as p:
-        p.starmap(merge_paper, args)
+        p.starmap(merge_and_write, args)
+    append_repeated(len(args)-1)
 
 
 if __name__ == '__main__':
-    main_paper()
+    main()
