44d74192ee86b793e2531140bc837d4ec8fc153d
henrik <henrik@unist.ac.kr>
2022-06-08 19:42:24 +0900

    Modify Some attributes of Journal and Article & beyond
    <Attr changes>
        1. Journal.titles & Journal.issns: {dtype -> set(values)} -> set(values of all dtypes)
        2. Journal.counts: Total number of distinct Articles each Journal contains
        3. Journal.extra_attrs: Any extra attributes to reside without breaking backward compatibility
        4. Journal.info: Method -> @property
        5. Article.journal: Removed and replaced with @property
        6. Article._journal_title: Journal.medline_ta of its journal
    <MetaCache>
    <Refactoring>

--- src/Papers/__main__.py
+++ src/Papers/__main__.py
@@ -3,5 +3,7 @@ if __name__ == '__main__':
     main0()
     from .main import main as main1
     main1()
-    from .merge import main as main2
+    from .replicas import main as main2
     main2()
+    from .merge import main as main3
+    main3()
