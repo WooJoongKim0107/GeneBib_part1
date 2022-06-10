c4c6f2fec3e5b0ae52edce636ffc030cc4479f14
henrik <henrik@unist.ac.kr>
2022-06-10 11:15:41 +0900

    Add Sorted collection of Articles by Journal

--- src/Papers/sort.py
+++ src/Papers/sort.py
@@ -2,16 +2,16 @@ import gzip, pickle, tarfile
 from multiprocessing import Pool
 from mypathlib import PathTemplate
 from Papers import Journal
-from Papers.article_finder import ArticleFinder
+from Papers.article_finder import ArticleFinder, JnlToPmids
 
 
-_W_FILE0 = PathTemplate('$rsrc/pdata/paper/papers.tar.gz')
+_W_FILE0 = PathTemplate('$rsrc/pdata/paper/papers.tar')
 _W_FILE1 = PathTemplate('$rsrc/pdata/paper/sorted/$journal.pkl.gz')
-assert _W_FILE0.substitute() == Journal._TAR_PATH
+assert str(_W_FILE0.substitute()) == str(Journal._TAR_PATH)
 assert str(_W_FILE1) == str(Journal._ARTICLE_PATH)
 
 
-def write(journal: Journal):
+def write(journal: Journal, pmids):
     if journal.path.is_file():
         print(journal.medline_ta, ': already done')
         return
@@ -20,7 +20,7 @@ def write(journal: Journal):
     with gzip.open(journal.path, 'wb') as file:
         pickle.dump(journal.counts+1, file)
         pickle.dump(key, file)
-        for article in ArticleFinder.from_journal(journal):
+        for article in ArticleFinder.from_pmids(*pmids):
             pickle.dump(article, file)
     print(key)
 
@@ -33,6 +33,10 @@ def tar():
 
 
 def main():
+    j2p = JnlToPmids()
+    args = [(journal, j2p[key]) for key, journal in Journal.unique_items()]
+    del j2p
+
     with Pool(5) as p:
-        p.map(write, Journal.unique_values())
+        p.starmap(write, args)
     tar()
