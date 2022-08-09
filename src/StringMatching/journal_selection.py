8f99883dd494072ac42bb5493d1a68b0744fa628
henrik <henrik@unist.ac.kr>
2022-08-09 18:12:35 +0900

    Modify journal_selection.py according to new information

--- src/StringMatching/journal_selection.py
+++ src/StringMatching/journal_selection.py
@@ -1,37 +1,32 @@
 import pickle
+import pandas as pd
 from Papers import Journal
 
-with open('./jcr_journals.pkl', 'rb') as file:
-    jcrs = {k.lower(): v for k, v in pickle.load(file).items()}
-
-with open('./field2jnl.pkl', 'rb') as file:
-    f2j = pickle.load(file)
+journals = pd.read_csv('./wos-core_SCIE_2022-April-19_selected.csv', header=0, index_col=0)
+journals = journals.T.to_dict()
 
 
-def foo(j):
-    match jcrs.get(j.lower(), {}):
+def foo(k, v):
+    match v:
         case {'ISSN': str(issn)} if res := list(Journal.find_issn(issn)):
             return 'ISSN', res
         case {'eISSN': str(issn)} if res := list(Journal.find_issn(issn)):
             return 'eISSN', res
-
-    if res := list(Journal.find_title(j, strict=True)):
-        return 'strict', res
+    if res := list(Journal.find_title(k, strict=True)):
+        return 'title', res
     else:
-        # There are two cases ('Algae', 'Sociobiology')
+        # There are 9 cases ('Algae', 'Sociobiology')
         #  where list(Journal.find_title(j, strict=False)) is not empty.
         # However, those results seems not proper.
         return 'failed', []
 
 
-q = {}
-for js in f2j.values():
-    for j in js:
-        if j not in q:
-            q[j] = foo(j)
-for j, (method, result) in q.items():
+q = {k: foo(k, v) for k, v in journals.items()}
+# Counter({'ISSN': 3696, 'eISSN': 81, 'title': 8, 'failed': 247})
+# Counter({'len(res)==1': 3785, 'len(res)==0': 247})
+for k, (method, result) in q.items():
     assert len(result) == 1 or method == 'failed'
-selected = {j: result[0].medline_ta for j, (method, result) in q.items() if method != 'failed'}
+selected = {k: result[0].medline_ta for k, (method, result) in q.items() if method != 'failed'}
 
 with open('./jnls_selected.pkl', 'wb') as file:
     pickle.dump(selected, file)
