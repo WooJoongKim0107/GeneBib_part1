233d6951c37cb461d72565b1614f835a02e2695c
henrik <henrik@unist.ac.kr>
2022-09-13 15:44:48 +0900

    Complete-blueprint of Community.containers

--- src/Community/containers.py
+++ src/Community/containers.py
@@ -1,9 +1,13 @@
+import gzip
+import pickle
 from UniProt.containers import UniProtEntry
 from myclass.cached_class import MetaCacheExt
 from mypathlib import PathTemplate
 
 
 class Community(metaclass=MetaCacheExt):
+    ENTRIES = {}
+    R_FILE = PathTemplate('$rsrc/pdata/uniprot/uniprot_sprot_parsed.pkl.gz').substitute()
     _CACHE_PATH = PathTemplate('$rsrc/pdata/community/community_cache.pkl.gz').substitute()
 
     def __new__(cls, cmnt_idx: int, caching=True):
@@ -12,9 +16,9 @@ class Community(metaclass=MetaCacheExt):
 
     def __init__(self, cmnt_idx: int, caching=True):
         self.cmnt_idx: int = cmnt_idx
-        self.entries: list[UniProtEntry] = []
-        self.added: list[str] = []
-        self.removed: list[str] = []
+        self.entries: list[str] = []
+        self.added_keywords: set[str] = set()
+        self.removed_keywords: set[str] = set()
         self.pmids: list[int] = []
 
     def __getnewargs__(self):
@@ -33,18 +37,71 @@ class Community(metaclass=MetaCacheExt):
         new.added = added
         new.removed = removed
 
-    @property
-    def key_accs(self):
-        return [ent.key_acc for ent in self.entries]
+    @classmethod
+    def load_entries(cls):
+        with gzip.open(cls.R_FILE, 'rb') as file:
+            cls.ENTRIES = pickle.load(file)
 
-    @property
-    def accessions(self):
-        return [acc for ent in self.entries for acc in ent.accessions]
+    def get_keywords(self, entries_dict=None):
+        entries_dict = self.ENTRIES if entries_dict is None else entries_dict
+        entries = [entries_dict[ent] for ent in self.entries]
+        keywords = {k for ent in entries for k in ent.keywords}
+        tobe_added = set().union(*(ent.added_keywords for ent in entries))
+        tobe_removed = set().union(*(ent.removed_keywords for ent in entries))
+        return keywords | tobe_added - tobe_removed
 
-    @property
-    def keywords(self):
-        return [key for ent in self.entries for key in ent.keywords]
 
-    @property
-    def references(self):
-        return [ref for ent in self.entries for ref in ent.references]
+# if Community._CACHE_PATH.is_file():
+#     Community.import_cache(verbose=True)
+
+
+class Ent2Cmnt(dict):
+    RW_FILE = PathTemplate('$rsrc/lite/community/ent2cmnt.pkl').substitute()
+
+    def __init__(self, load=True):
+        if load:
+            data = self.load()
+        else:
+            data = self.generate()
+        super().__init__(data)
+
+    @classmethod
+    def load(cls):
+        with open(cls.RW_FILE, 'rb') as file:
+            return pickle.load(file)
+
+    @staticmethod
+    def generate():
+        Community.import_cache_if_empty(verbose=True)
+        for cmnt_idx, cmnt in Community.items():
+            for ent in cmnt.entries:
+                yield ent, cmnt_idx
+
+
+class Key2Cmnt(dict):
+    _R_FILE = PathTemplate('$rsrc/pdata/uniprot/uniprot_sprot_parsed.pkl.gz').substitute()
+    RW_FILE = PathTemplate('$rsrc/lite/community/key2cmnt.pkl').substitute()
+
+    def __init__(self, load=True):
+        if load:
+            data = self.load()
+        else:
+            data = self.generate()
+        super().__init__(data)
+
+    @classmethod
+    def load(cls):
+        with open(cls.RW_FILE, 'rb') as file:
+            return pickle.load(file)
+
+    @classmethod
+    def generate(cls):
+        Community.import_cache_if_empty(verbose=True)
+        Community.load_entries()
+        for cmnt in Community.values():
+            for key in cmnt.get_keywords():
+                yield key, cmnt
+
+    def dump(self):
+        with open(self.RW_FILE, 'wb') as file:
+            pickle.dump(dict(self), file)
