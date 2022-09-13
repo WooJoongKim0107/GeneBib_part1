0eafedf7c8422fc41d021a7338eaa1e39bc9f58f
henrik <henrik@unist.ac.kr>
2022-09-13 15:42:37 +0900

    Add string-unifying function used for clustering UniProt KeyWords

--- src/StringMatching/base.py
+++ src/StringMatching/base.py
@@ -26,3 +26,20 @@ def tokenize(s: str):
     span_phrase = iter([match.span(), match.group()] for match in finditer)
     indices, tokens = zip(*span_phrase)
     return indices, tokens
+
+
+def uniform_keyw_2(x):
+    keywLength = len(x)
+    outputKeyw = x
+    outputKeyw = re.sub(' ?, ?', ',', outputKeyw)
+    outputKeyw = re.sub('\[', '(', outputKeyw)
+    outputKeyw = re.sub('\]', ')', outputKeyw)
+    outputKeyw = re.sub('(?i)^probable ', '', outputKeyw)
+    outputKeyw = re.sub('(?i)^putative ', '', outputKeyw)
+    outputKeyw = re.sub('(?i) homolog(,? \w+)?$', '', outputKeyw)
+    outputKeyw = re.sub('(?i) isoform(,? \w+)?$', '', outputKeyw)
+    if keywLength > 4:
+        outputKeyw = outputKeyw.lower()
+    elif keywLength > 2:
+        outputKeyw = outputKeyw[0].upper()+outputKeyw[1:]
+    return outputKeyw
