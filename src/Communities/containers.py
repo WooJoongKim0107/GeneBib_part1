12a8282d7b404db717e933154441545b944b53e5
henrik <henrik@unist.ac.kr>
2022-10-26 17:09:18 +0900

    Feature added on Community with some refactoring

--- src/Communities/containers.py
+++ src/Communities/containers.py
@@ -1,9 +1,12 @@
+from random import choices
 from collections.abc import Iterable
 from textwrap import dedent, indent
 from myclass import MetaCacheExt, MetaDisposal, TarRW
 from mypathlib import PathTemplate
 from UniProt.containers import Match, KeyWord
 from StringMatching.base import unify, tokenize
+from Papers.containers import Article
+from Patents.containers import Patent
 
 
 class Community(metaclass=MetaCacheExt):
@@ -11,19 +14,128 @@ class Community(metaclass=MetaCacheExt):
     ARTICLE_PATH = PathTemplate('$pdata/paper/sorted/community.tar').substitute()
     PATENT_PATH = PathTemplate('$pdata/patent/sorted/community.tar').substitute()
 
-    def __repr__(self):
-        return f"Community({self.cmnt_idx})"
-
-    def __new__(cls, cmnt_idx: int, caching=True):
-        """__new__ method must not be skipped - Assertion of MetaCacheExt"""
-        return super().__new__(cls)
-
     def __init__(self, cmnt_idx: int, caching=True):
         self.cmnt_idx: int = cmnt_idx
         self.entries: list[str] = []
         self.keywords: set[str] = set()
         self.pmids: dict[str, set[int]] = {}
         self.pub_numbers: dict[str, set[str]] = {}
+
+    def choices(self, k, *texts):
+        if texts:
+            pmids = [pmid for text in texts for pmid in self.pmids[text]]
+            pubs = [pub for text in texts for pub in self.pub_numbers[text]]
+        else:
+            pmids = [x for v in self.pmids.values() for x in v]
+            pubs = [x for v in self.pub_numbers.values() for x in v]
+        return choices(pmids, k=k), choices(pubs, k=k)
+
+    def random_materials(self, k, *texts):
+        pmids, pubs = map(set, self.choices(k, *texts))
+        arts = [art for art in self.get_articles() if art.pmid in pmids]
+        pats = [pat for pat in self.get_patents() if pat.pub_number in pubs]
+        return arts, pats
+
+    @property
+    def best_known_as(self):
+        hs = self.hit_summary()
+        if hs:
+            return next(iter(hs))
+        else:
+            return ''
+
+    @property
+    def less_info(self):
+        res = self.__repr__() + ':  '
+        for name in self.hit_summary():
+            res += name + ', '
+        return res
+
+    @property
+    def info(self):
+        return dedent(f"""\
+        {self}, bka., {self.best_known_as}
+               Entries: {_print_set(self.entries)}
+              Keywords: {_print_set(self.keywords)}
+            Total hits:
+                        {self.total_paper_hits:>7,} hits (paper)
+                        {self.total_patent_hits:>7,} hits (patent)
+        """)
+
+    @property
+    def more_info(self):
+        return dedent(f"""\
+        {self}
+               Entries: {_print_set(self.entries)}
+              Keywords: {_print_set(self.keywords)}
+            Total hits: 
+                        {self.total_paper_hits:>7,} hits (paper)
+                        {self.total_patent_hits:>7,} hits (patent)
+               Details:
+        """) + self._more_info() + '\n' if self else self.info
+
+    @property
+    def paper_hits(self) -> dict[str, int]:
+        """{match.text -> num_hits}"""
+        return {k: len(v) for k, v in self.pmids.items()}
+
+    @property
+    def total_paper_hits(self) -> int:
+        """return total number of paper hits"""
+        return sum(self.paper_hits.values())
+
+    def hits(self, text: str) -> tuple[int, int]:
+        """return (total_paper_hits, total_patent_hits)"""
+        return self.paper_hits.get(text, 0), self.patent_hits.get(text, 0)
+
+    @property
+    def total_hits(self) -> int:
+        """return total number of hits (regardless of material type)"""
+        return self.total_paper_hits + self.total_patent_hits
+
+    def hit_summary(self) -> dict[str, tuple[int, int]]:
+        """{match.text -> (total_paper_hits, total_patent_hits)} sorted by paper_hits and patent_hits"""
+        keys = sorted(self.paper_hits | self.patent_hits, key=self.hits, reverse=True)
+        return {k: self.hits(k) for k in keys}
+
+    def get_articles(self) -> list[Article]:
+        if not self.total_paper_hits:
+            return []
+        with TarRW(self.ARTICLE_PATH, 'r') as tf:
+            return tf.gzip_load(f'{self.cmnt_idx}.pkl.gz')  # list[Article]
+
+    @property
+    def patent_hits(self) -> dict[str, int]:
+        """dict[match.text, num_hits]"""
+        return {k: len(v) for k, v in self.pub_numbers.items()}
+
+    @property
+    def total_patent_hits(self) -> int:
+        return sum(self.patent_hits.values())
+
+    def get_patents(self) -> list[Patent]:
+        if not self.total_patent_hits:
+            return []
+        with TarRW(self.PATENT_PATH, 'r') as tf:
+            return tf.gzip_load(f'{self.cmnt_idx}.pkl.gz')  # list[Patent]
+
+    # Fine-details
+    def __repr__(self):
+        return f"Community({self.cmnt_idx})"
+
+    def __bool__(self):
+        return bool(self.total_hits)
+
+    def _more_info(self):
+        summary = self.hit_summary()
+        form = '{:,}'.format
+        ps, ts = zip(*(map(form, x) for x in summary.values()))
+        return indent(col_prints(summary, ps, ts, sep=' '), ' '*16)
+
+    # Generation of instances & cache
+    def __new__(cls, cmnt_idx: int, caching=True):
+        """__new__ method must not be skipped - Assertion of MetaCacheExt"""
+        return super().__new__(cls)
 
     def __getnewargs__(self):
         """__getnewargs__ must not be deleted - Assertion of MetaCacheExt"""
@@ -43,79 +155,11 @@ class Community(metaclass=MetaCacheExt):
         new.entries = list(entries)
         new.keywords = UnifyEqKeys.all_equivalents(unified_keywords)
 
-    @property
-    def paper_hits(self):
-        return {k: len(v) for k, v in self.pmids.items()}
-
-    @property
-    def patent_hits(self):
-        return {k: len(v) for k, v in self.pub_numbers.items()}
-
-    @property
-    def total_paper_hits(self):
-        return sum(self.paper_hits.values())
-
-    @property
-    def total_patent_hits(self):
-        return sum(self.patent_hits.values())
-
-    def count_hits(self, text: str):
-        return self.paper_hits.get(text, 0), self.patent_hits.get(text, 0)
-
-    @property
-    def total_hits(self):
-        return self.total_paper_hits + self.total_patent_hits
-
-    def hit_summary(self):
-        keys = sorted(self.paper_hits | self.patent_hits, key=self.count_hits, reverse=True)
-        return {k: self.count_hits(k) for k in keys}
-
-    @property
-    def info(self):
-        return dedent(f"""\
-        {self}
-               Entries: {_print_set(self.entries)}
-              Keywords: {_print_set(self.keywords)}
-            Total hits:
-                        {self.total_paper_hits:>7,} hits (paper)
-                        {self.total_patent_hits:>7,} hits (patent)
-        """)
-
-    @property
-    def more_info(self):
-        if not self.total_hits:
-            return self.info
-        summary = self.hit_summary()
-        form = '{:,}'.format
-        ps, ts = zip(*(map(form, x) for x in summary.values()))
-        details = indent(col_prints(summary, ps, ts, sep=' '), ' '*16)
-
-        return dedent(f"""\
-        {self}
-               Entries: {_print_set(self.entries)}
-              Keywords: {_print_set(self.keywords)}
-            Total hits: 
-                        {self.total_paper_hits:>7,} hits (paper)
-                        {self.total_patent_hits:>7,} hits (patent)
-               Details:
-        """) + details + '\n'
-
     @classmethod
     def assign(cls, cmnt_idx, attr, key, match):
+        """Method where self.pmids & self.pub_numbers are extended"""
         self = cls(cmnt_idx)
         getattr(self, attr).setdefault(match.text, set()).add(key)
-
-    def get_articles(self):
-        if not self.total_paper_hits:
-            return []
-        with TarRW(self.ARTICLE_PATH, 'r') as tf:
-            return tf.gzip_load(f'{self.cmnt_idx}.pkl.gz')  # list[Article]
-
-    def get_patents(self):
-        if not self.total_patent_hits:
-            return []
-        with TarRW(self.PATENT_PATH, 'r') as tf:
-            return tf.gzip_load(f'{self.cmnt_idx}.pkl.gz')  # list[Patent]
 
 
 class UnifyEqKeys(metaclass=MetaDisposal):
